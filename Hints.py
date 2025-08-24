c4370cc1f21b2fb9cfa023c35d46ed7ce7f189b6
jdunn596 <82658083+jdunn596@users.noreply.github.com>
2025-08-24 10:08:04 -0400

    remove doubled color tag

--- Hints.py
+++ Hints.py
@@ -1191,7 +1191,7 @@ def get_important_check_hint(spoiler: Spoiler, world: World, checked: set[str]) 
     else:
         numcolor = 'Green'
 
-    return GossipText('#%s# has #%d# major item%s.' % (hint_area.text(world.settings.clearer_hints), item_count, "s" if item_count != 1 else ""), ['Green', numcolor]), None
+    return GossipText('%s has #%d# major item%s.' % (hint_area.text(world.settings.clearer_hints), item_count, "s" if item_count != 1 else ""), ['Green', numcolor]), None
 
 
 hint_func: dict[str, HintFunc | BarrenFunc] = {
